501809db2f73e1ab8ba550b324bd54a826565c6b
amolmane7471 <maneamol3233@gmail.com>
2022-08-06 13:39:54 +0530

    [Amol]:added line computation program to display welcome msg

---
+++ src/com/bridgelabz/linecomputation/LineComputation.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.linecomputation;
+public class LineComputation {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome To Line Comparison Computation Program");
+
+	}
+
+}
